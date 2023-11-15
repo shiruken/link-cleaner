4c9d874835e6f9ad8dff503cc88c37167bdce26e
Colin Sullender <shiruken343@gmail.com>
2023-11-15 14:38:00 -0700

    v0.1

---
+++ README.md
@@ -0,0 +1,9 @@
+# Link Cleaner
+
+Frustrated with the new shortened share link format that hides submission details and obfuscates tracker tags? Use Link Cleaner to easily copy a sanitized permalink to a post or comment.
+
+https://developers.reddit.com/apps/link-cleaner
+
+## How to Use
+
+Just click the menu icon on a post or comment and select "Get Clean Link" to reveal the sanitized permalink.
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,51 @@
-// Visit developers.reddit.com/docs to learn Devvit!
-
 import { Devvit } from '@devvit/public-api';
 
+Devvit.configure({
+  redditAPI: true,
+});
+
 Devvit.addMenuItem({
-  location: 'post',
-  label: 'Hello World',
-  onPress: (event, context) => {
-    console.log(`Pressed ${event.targetId}`);
-    context.ui.showToast('Hello world!');
+  location: ['post', 'comment'],
+  label: 'Get Clean Link',
+  onPress: async (event, context) => {
+    const id = event.targetId;
+    console.log(id);
+
+    let permalink: string;
+    if (id.includes("t3_")) {
+      const post = await context.reddit.getPostById(id);
+      permalink = post.permalink;
+    } else if (id.includes("t1_")) {
+      const comment = await context.reddit.getCommentById(id);
+      permalink = comment.permalink;
+    } else {
+      context.ui.showToast({
+        appearance: "neutral", // No error appearance yet
+        text: "Something went wrong. Please try again."
+      });
+      throw new Error(`Object is not a post or comment: ${id}`);
+    }
+
+    const formData = {
+      url: `https://www.reddit.com${permalink}`
+    };
+    context.ui.showForm(form, formData);
   },
+});
+
+const form = Devvit.createForm((data) => {
+  return {
+    fields: [{ 
+      name: 'url',
+      label: 'Clean URL',
+      type: 'string',
+      defaultValue: data.url,
+      helpText: 'Permalink without Reddit tracking tags',
+      disabled: false
+    }],
+  }
+}, () => {
+  // pass
 });
 
 export default Devvit;
